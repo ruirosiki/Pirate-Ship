9603dc3dadda6ab7585dcdbda6b33d4ad33d62d9
ruirosiki <mr.andrew.corbin@icloud.com>
2023-04-26 12:37:50 -0600

    //start computer move function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // REQUIRED CONSTANTS
-
 //players constant
 const PLAYERS = {
   player: "player",
@@ -14,6 +13,7 @@ const BOARD_SQUARE = {
   "-1": -1, //miss
   "-2": -2, //hit ship
 };
+
 //defines a class for ship - allows to create ships of different lengths
 class Ship {
   constructor(length, hitCount, isSunk, location) {
@@ -23,13 +23,17 @@ class Ship {
     this.location = [];
   }
 }
+
+//defines a ships object to hold the different types of ships
 const SHIPS = {
   dinghy: new Ship(2, 0, false),
   sloop: new Ship(3, 0, false),
   galleon: new Ship(4, 0, false),
   queenAnnesRevenge: new Ship(5, 0, false),
 };
-//defines a class for the board - allows creation of separate boards (computer and player or eventually player1 and player2)
+
+//defines a class for the board
+//allows creation of separate boards (computer and player or eventually player1 and player2)
 class Board {
   constructor() {
     this.board = [
@@ -46,14 +50,16 @@ class Board {
     ];
   }
 }
-
 // console.log(SHIPS.dinghy);
 // console.log(SHIPS.sloop);
 // console.log(SHIPS.galleon);
 // console.log(SHIPS.queenAnnesRevenge);
+
 //win condition
 //eventually make this dynamic by determining the total max hits by calculating the total number of ships lengths
 const MAX_HITS = 17;
+
+//messages object to store messageEl content
 const MESSAGES = {
   pTurn: "Player's Turn",
   cTurn: "Computer's Turn",
@@ -64,7 +70,6 @@ const MESSAGES = {
   cWin: "All your ships are sunk. The computer wins",
   playAgain: "Press the play again button to have another go!",
 };
-// console.log(MESSAGES.cTurn);
 
 //STATE VARIABLES
 let playerBoard;
@@ -74,31 +79,26 @@ let winner;
 let shipLocation;
 
 //CACHED DOM ELEMENTS
-
 const messageEl = document.querySelector("#message");
-// console.log(messageEl);
 const playAgainButton = document.querySelector("button");
-// console.log(playAgainButton);
 const boardEls = [...document.querySelectorAll("#board > div")];
-// console.log(boardEls);
 
-//Event listeners
+//EVENT LISTENERS
 document.getElementById("board").addEventListener("click", playerClick);
 document.getElementById("play-again").addEventListener("click", init);
 
-//functions
+//FUNCTIONS
+//initialize game
 init();
-
 function init() {
-  playerBoard = new Board();
-  computerBoard = new Board();
   initPlayerBoard();
   initComputerBoard();
   turn = PLAYERS.player;
   winner = false;
   render();
 }
-//render
+
+//render the DOM elements
 function render() {
   renderTurnOrder();
   renderPlayAgain();
@@ -113,6 +113,7 @@ function renderBoard() {
     renderPlayerBoard();
   }
 }
+
 //render play again button
 function renderPlayAgain() {
   playAgainButton.style.visibility = winner ? "visible" : "hidden";
@@ -150,23 +151,28 @@ function playerClick(event) {
 }
 
 //computer move
+function computerMove() {
+  let computerGuesses = [];
+  let colCoordinate = Math.floor(Math.random() * 10);
+  let rowCoordinate = Math.floor(Math.random() * 100);
+  let computerGuess = rowCoordinate + colCoordinate;
+  for (guess of computerGuesses) {
+    if (computerGuess === guess) {
+      return;
+    } else if (computerGuess !== guess) {
+      computerGuesses.push(computerGuess);
+    }
+  }
+}
 
-// function computerMove() {
-//   let hitRow = Math.floor(Math.random() * 10);
-//   let hitCol = Math.floor(Math.random() * 10);
-//   while (playerBoard.board[hitRow][hitCol] !=== null) {
-
-//   }
-// }
-
-//if it is legal, add into boardArray
-
+//randomly places 4 ships in a horizontal direction
+//small bug: will randomly skip placing ship if too many tries
 function horizontalPlacement(board, ship, squareValue) {
   //generate two random start coordinates
   let colCoordinate = Math.floor(Math.random() * 10);
   let rowCoordinate = Math.floor(Math.random() * 10);
   let isValid = false;
-  console.log(rowCoordinate, colCoordinate);
+  // console.log(rowCoordinate, colCoordinate);
   if (
     board[rowCoordinate][colCoordinate] === null &&
     colCoordinate <= board.length - ship.length
@@ -187,11 +193,7 @@ function horizontalPlacement(board, ship, squareValue) {
     }
   } else horizontalPlacement(board, ship, squareValue);
 }
-// horizontalPlacement(playerBoard.board, SHIPS.dinghy, 1);
-// horizontalPlacement(playerBoard.board, SHIPS.sloop, 1);
-// horizontalPlacement(playerBoard.board, SHIPS.galleon, 1);
-// horizontalPlacement(playerBoard.board, SHIPS.queenAnnesRevenge, 1);
-// horizontalPlacement(computerBoard.board, dinghy, 2);
+
 console.log("pboard", playerBoard);
 console.log("cboard", computerBoard);
 
@@ -260,12 +262,14 @@ function isHit(clickedSquare) {
 
 //init playerBoard and computer board
 function initPlayerBoard() {
+  playerBoard = new Board();
   horizontalPlacement(playerBoard.board, SHIPS.dinghy, 1);
   horizontalPlacement(playerBoard.board, SHIPS.sloop, 1);
   horizontalPlacement(playerBoard.board, SHIPS.galleon, 1);
   horizontalPlacement(playerBoard.board, SHIPS.queenAnnesRevenge, 1);
 }
 function initComputerBoard() {
+  computerBoard = new Board();
   horizontalPlacement(computerBoard.board, SHIPS.dinghy, 2);
   horizontalPlacement(computerBoard.board, SHIPS.sloop, 2);
   horizontalPlacement(computerBoard.board, SHIPS.galleon, 2);
